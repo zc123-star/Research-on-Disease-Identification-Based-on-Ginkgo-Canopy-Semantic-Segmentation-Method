9ec0166d0479728dff88be17b4f02088b686d0b6
zc123-star <290384920@qq.com>
2025-10-29 11:17:53 +0800

    Update train_multi_GPU.py

--- train_multi_GPU.py
+++ train_multi_GPU.py
@@ -4,7 +4,7 @@ import datetime
 
 import torch
 
-from src import UNet
+from src import multi_unet
 from train_utils import train_one_epoch, evaluate, create_lr_scheduler, init_distributed_mode, save_on_master, mkdir
 from my_dataset import DriveDataset
 import transforms as T
@@ -69,7 +69,7 @@ def main(args):
     mean = (0.709, 0.381, 0.224)
     std = (0.127, 0.079, 0.043)
 
-    # 用来保存coco_info的文件
+    # File used to save coco_info
     results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
     data_root = args.data_path
@@ -124,15 +124,15 @@ def main(args):
 
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
 
-    # 创建学习率更新策略，这里是每个step更新一次(不是每个epoch)
+    # Create a learning rate update strategy, here it is updated every step (not every epoch)
     lr_scheduler = create_lr_scheduler(optimizer, len(train_data_loader), args.epochs, warmup=True)
 
-    # 如果传入resume参数，即上次训练的权重地址，则接着上次的参数训练
+    # If the resume parameter is passed, which is the address of the last training weights, training will continue from the previous parameters.
     if args.resume:
         # If map_location is missing, torch.load will first load the module to CPU
         # and then copy each parameter to where it was saved,
         # which would result in all processes on the same machine using the same set of devices.
-        checkpoint = torch.load(args.resume, map_location='cpu')  # 读取之前保存的权重文件(包括优化器以及学习率策略)
+        checkpoint = torch.load(args.resume, map_location='cpu')  # Load the previously saved weight file (including the optimizer and learning rate schedule)
         model_without_ddp.load_state_dict(checkpoint['model'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
@@ -160,11 +160,11 @@ def main(args):
         print(val_info)
         print(f"dice coefficient: {dice:.3f}")
 
-        # 只在主进程上进行写操作
+        # Write operations are performed only in the main process.
         if args.rank in [-1, 0]:
             # write into txt
             with open(results_file, "a") as f:
-                # 记录每个epoch对应的train_loss、lr以及验证集各指标
+                # Record the train_loss, learning rate (lr), and various metrics of the validation set for each epoch.
                 train_info = f"[epoch: {epoch}]\n" \
                              f"train_loss: {mean_loss:.4f}\n" \
                              f"lr: {lr:.6f}\n" \
@@ -178,7 +178,7 @@ def main(args):
                 continue
 
         if args.output_dir:
-            # 只在主节点上执行保存权重操作
+            # Perform the weight-saving operation only on the master node
             save_file = {'model': model_without_ddp.state_dict(),
                          'optimizer': optimizer.state_dict(),
                          'lr_scheduler': lr_scheduler.state_dict(),
@@ -205,44 +205,28 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__)
 
-    # 训练文件的根目录(DRIVE)
     parser.add_argument('--data-path', default='./', help='dataset')
-    # 训练设备类型
     parser.add_argument('--device', default='cuda', help='device')
-    # 检测目标类别数(不包含背景)
     parser.add_argument('--num-classes', default=1, type=int, help='num_classes')
-    # 每块GPU上的batch_size
     parser.add_argument('-b', '--batch-size', default=4, type=int,
                         help='images per gpu, the total batch size is $NGPU x batch_size')
-    # 指定接着从哪个epoch数开始训练
     parser.add_argument('--start_epoch', default=0, type=int, help='start epoch')
-    # 训练的总epoch数
     parser.add_argument('--epochs', default=200, type=int, metavar='N',
                         help='number of total epochs to run')
-    # 是否使用同步BN(在多个GPU之间同步)，默认不开启，开启后训练速度会变慢
     parser.add_argument('--sync_bn', type=bool, default=False, help='whether using SyncBatchNorm')
-    # 数据加载以及预处理的线程数
     parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
-    # 训练学习率，这里默认设置成0.01(使用n块GPU建议乘以n)，如果效果不好可以尝试修改学习率
     parser.add_argument('--lr', default=0.01, type=float,
                         help='initial learning rate')
-    # SGD的momentum参数
     parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                         help='momentum')
-    # SGD的weight_decay参数
     parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
                         metavar='W', help='weight decay (default: 1e-4)',
                         dest='weight_decay')
-    # 只保存dice coefficient值最高的权重
     parser.add_argument('--save-best', default=True, type=bool, help='only save best weights')
-    # 训练过程打印信息的频率
     parser.add_argument('--print-freq', default=1, type=int, help='print frequency')
-    # 文件保存地址
     parser.add_argument('--output-dir', default='./multi_train', help='path where to save')
-    # 基于上次的训练结果接着训练
     parser.add_argument('--resume', default='', help='resume from checkpoint')
-    # 不训练，仅测试
     parser.add_argument(
         "--test-only",
         dest="test_only",
@@ -250,17 +234,14 @@ if __name__ == "__main__":
         action="store_true",
     )
 
-    # 分布式进程数
     parser.add_argument('--world-size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
-    # Mixed precision training parameters
     parser.add_argument("--amp", default=False, type=bool,
                         help="Use torch.cuda.amp for mixed precision training")
 
     args = parser.parse_args()
 
-    # 如果指定了保存文件地址，检查文件夹是否存在，若不存在，则创建
     if args.output_dir:
         mkdir(args.output_dir)
 
